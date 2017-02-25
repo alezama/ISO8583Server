53c1348523f40a4cf814d111f36fc4978b628f05
anuar Lezama Barquet <anuar Lezama Barquet>
2017-02-25 00:13:22 -0600

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import com.sun.org.apache.xalan.internal.xsltc.trax.SmartTransformerFactoryImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by estoe_000 on 2/24/2017.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: estoe_000
+  Date: 2/24/2017
+  Time: 10:41 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+  <p>Get your greeting <a href="/greeting">here</a></p>
+  </body>
+</html>
